d2c4414a52d04e279da15fa13850ca28255c306b
Tim Harrison <heedspin@gmail.com>
2013-09-14 10:03:09 -0400

    Fix email party loading.

--- app/serializers/email_summary_serializer.rb
+++ app/serializers/email_summary_serializer.rb
@@ -1,7 +1,7 @@
 class EmailSummarySerializer < ActiveModel::Serializer
 	embed :ids, include: true
   attributes :id, :date, :subject
-  has_many :parties
+  has_many :parties, key: :parties
   has_many :to_email_accounts, root: :email_accounts, serializer: EmailAccountSerializer, key: :to_email_accounts
   has_many :from_email_accounts, root: :email_accounts, serializer: EmailAccountSerializer, key: :from_email_accounts
   has_many :cc_email_accounts, root: :email_accounts, serializer: EmailAccountSerializer, key: :cc_email_accounts
